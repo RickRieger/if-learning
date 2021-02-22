5e4f4d4c066b095c6b956c1f910c32c1744b8625
Rick Rieger <rickrieger36251@gmail.com>
2021-02-22 01:48:11 -0500

    finished

--- main.js
+++ main.js
@@ -3,13 +3,61 @@
  ******************************/
 
 
+function isItLong(input){
+  let n = input.length;
+  if (n > 20){
+    return "That's a long string!"; 
+  }
+}
 
+function isItMedium(input){
+  let n = input.length;
+  if(n >= 10 && n <= 20){
+    return "That's a regular sized string!";
+  }
+}
+
+function isItShort(input){
+    let n = input.length;
+    if(n < 10){
+      return "That's a small string!";
+    }
+    else{
+      return "That's not a small string!";
+    }
+}
+
+function howLongIsMyString(input){
+  let n = input.length;
+  if (n > 20){
+    return "That's a long string!"; 
+  }
+  else if(n >= 10 && n <= 20){
+    return "That's a regular sized string!";
+  }
+  else  if(n < 10){
+    return "That's a small string!";
+  }
+}
+
+function instructorHeight(input){
+
+  if (input === 'Colin'){
+    return 62; 
+  }
+  else if (input === 'Mesuara'){
+    return 69;
+  }
+  else
+    return "I don't know that instructor!";
+  
+}
 
 
 
 /**********************************
- *      OUR CODE GOES BELOW.      *
- * THIS IS FOR INTERNAL USE ONLY. *
+ *   
+ *  * THIS IS FOR INTERNAL USE ONLY. *
  * DO NOT ADD OR CHANGE ANYTHING! *
  **********************************/
 
